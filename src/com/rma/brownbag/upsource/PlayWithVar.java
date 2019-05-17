5611757364db1ebb73d4e3b53e78ae7c8e4bedb4
Adam Korynta <adam@rmanet.com>
2019-05-16 18:24:51 -0700

    fix curly brace placement

---
+++ src/com/rma/brownbag/upsource/PlayWithVar.java
@@ -0,0 +1,18 @@
+package com.rma.brownbag.upsource;
+
+import java.util.ArrayList;
+
+/**
+ * Company: Resource Management Associates
+ *
+ * @author <a href="mailto:adam@rmanet.com">Adam Korynta</a>
+ * @since 05-16-2019
+ */
+public class PlayWithVar
+{
+	public void printSomeVar()
+	{
+		var list = new ArrayList<String>();
+		System.out.println(list);
+	}
+}
