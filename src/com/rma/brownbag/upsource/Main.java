8b6daec6c5918c2edbeb66958f9714a18d87adba
Adam Korynta <adam@rmanet.com>
2019-05-16 18:22:59 -0700

    fix curly brace placement

--- src/com/rma/brownbag/upsource/Main.java
+++ src/com/rma/brownbag/upsource/Main.java
@@ -2,10 +2,11 @@ package com.rma.brownbag.upsource;
 
 import java.util.logging.Logger;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args)
-    {
-        Logger.getLogger(Main.class.getName()).info("Hello World!");
-    }
+	public static void main(String[] args)
+	{
+		Logger.getLogger(Main.class.getName()).info("Hello World!");
+	}
 }
