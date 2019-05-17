99f2c724a0a77321bdaede680cd7e17c8e3f4011
Adam Korynta <adam@rmanet.com>
2019-05-16 18:56:48 -0700

    add print iterator

--- src/main/java/com/rma/brownbag/Main.java
+++ src/main/java/com/rma/brownbag/Main.java
@@ -1,5 +1,6 @@
 package com.rma.brownbag;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 public class Main
@@ -8,5 +9,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		Logger.getLogger(Main.class.getName()).info("Hello World!");
+		Iterate iterate = new Iterate(new ArrayList<String>());
+		iterate.printFirst();
 	}
 }
