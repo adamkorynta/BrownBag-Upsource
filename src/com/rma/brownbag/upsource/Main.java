29224cbd11fa3c8f7708c2e55c8df53860afd13f
Adam Korynta <adam@rmanet.com>
2019-05-16 18:05:32 -0700

    using logger instead of System.out

--- src/com/rma/brownbag/upsource/Main.java
+++ src/com/rma/brownbag/upsource/Main.java
@@ -1,9 +1,11 @@
 package com.rma.brownbag.upsource;
 
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        Logger.getLogger(Main.class.getName()).info("Hello World!");
     }
 }
